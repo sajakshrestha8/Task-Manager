3320a64a4deab967fb1ba4cbfb460d941582c64a
sajakshrestha8 <shresthasajak123@gmail.com>
2025-04-07 10:55:40 +0545

    login route working

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,10 +1,13 @@
 const { response } = require("express");
 const connection = require("../config/database/connection");
 const user = require("../models/user");
+const { where } = require("sequelize");
+const bcrypt = require("bcrypt");
 
 const UserController = {
   RegisterUser: async (request, response) => {
     const { username, email, password } = request.body;
+    const hashPass = await bcrypt.hash(password, 10);
     try {
       await connection
         .sync()
@@ -12,7 +15,7 @@ const UserController = {
           return user.create({
             UserName: username,
             Email: email,
-            Password: password,
+            Password: hashPass,
           });
         })
         .then((data) => {
@@ -23,7 +26,32 @@ const UserController = {
     }
   },
 
-  LogInUser: async (request, response) => {},
+  LogInUser: async (request, response) => {
+    const { email, password } = request.body;
+
+    try {
+      let User = await user.findOne({
+        where: {
+          Email: email,
+        },
+      });
+
+      if (!User) {
+        return response.send("User not found");
+      } else {
+        let hashPass = await bcrypt.compare(password, User.Password);
+
+        console.log(hashPass);
+        if (hashPass) {
+          response.send("login successfull");
+        } else {
+          response.send("Wrong password");
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  },
 };
 
 module.exports = UserController;
--- server/routes/user/user.route.js
+++ server/routes/user/user.route.js
@@ -3,5 +3,6 @@ const UserController = require("../../controllers/user.controller");
 const router = app.Router();
 
 router.post("/register", UserController.RegisterUser);
+router.post("/login", UserController.LogInUser);
 
 module.exports = router;
