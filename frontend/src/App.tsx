038ca2b8787de05744f89d7a11930847f7964378
sajakshrestha8 <shresthasajak123@gmail.com>
2025-04-07 22:45:33 +0545

    create and delete working properly

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -13,16 +13,20 @@ import {
   DialogFooter,
   DialogHeader,
 } from "./components/ui/dialog";
-import { Copy } from "lucide-react";
 import { useNavigate } from "react-router-dom";
+import { Input } from "./components/ui/input";
 
 interface ITaskResponse {
+  Id: number;
   Title: string;
   Description: string;
 }
 
 const App = () => {
   const [task, setTask] = useState<ITaskResponse[] | []>([]);
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [show, setShow] = useState(false);
 
   const navigate = useNavigate();
 
@@ -47,6 +51,54 @@ const App = () => {
       console.log(error);
     }
   }, [URL, token, navigate]);
+
+  const handleAddTask = async () => {
+    try {
+      const res = await axios.post(
+        URL,
+        {
+          title: title,
+          description: description,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      setShow(!show);
+      fetchData();
+      console.log(res);
+    } catch (error) {
+      if (error instanceof AxiosError) {
+        console.log(error.message);
+      }
+    }
+  };
+
+  const handleDelete = async (id: number) => {
+    try {
+      const res = await axios.delete(`${URL}/${id}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      fetchData();
+      console.log(res);
+    } catch (error) {
+      if (error instanceof AxiosError) {
+        console.log(error.message);
+      }
+    }
+  };
+
+  const handleTitleInput = (value: string) => {
+    setTitle(value);
+  };
+
+  const handleDescInput = (value: string) => {
+    setDescription(value);
+  };
 
   useEffect(() => {
     fetchData();
@@ -79,12 +131,13 @@ const App = () => {
               <tr key={index}>
                 <td>{value.Title}</td>
                 <td>{value.Description}</td>
+                <Button onClick={() => handleDelete(value.Id)}>Delete</Button>
               </tr>
             );
           })}
         </tbody>
       </table>
-      <Dialog>
+      <Dialog open={show} onOpenChange={() => setShow(!show)}>
         <DialogTrigger asChild>
           <Button variant="outline">Create Task</Button>
         </DialogTrigger>
@@ -92,22 +145,44 @@ const App = () => {
           <DialogHeader>
             <DialogTitle>Create new task</DialogTitle>
             <DialogDescription>
-              Anyone who has this link will be able to view this.
+              Enter the data on all the required fields
             </DialogDescription>
           </DialogHeader>
-          <div className="flex items-center space-x-2">
-            <div className="grid flex-1 gap-2"></div>
-            <Button type="submit" size="sm" className="px-3">
-              <span className="sr-only">Copy</span>
-              <Copy />
-            </Button>
-          </div>
-          <DialogFooter className="sm:justify-start">
+          <form>
+            <label htmlFor="title">Title</label>
+            <Input
+              type="text"
+              name="title"
+              placeholder="Title of the task"
+              required
+              onChange={(e) => {
+                handleTitleInput(e.target.value);
+              }}
+            />
+            <label htmlFor="title">Description</label>
+            <Input
+              type="text"
+              placeholder="Description of the task"
+              required
+              onChange={(e) => {
+                handleDescInput(e.target.value);
+              }}
+            />
+          </form>
+          <DialogFooter className="sm:justify-end">
             <DialogClose asChild>
               <Button type="button" variant="secondary">
                 Close
               </Button>
             </DialogClose>
+            <Button
+              onClick={(e) => {
+                e.preventDefault();
+                handleAddTask();
+              }}
+            >
+              Add
+            </Button>
           </DialogFooter>
         </DialogContent>
       </Dialog>
